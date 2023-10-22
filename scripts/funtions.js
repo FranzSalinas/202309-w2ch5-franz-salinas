9e4a6fa36190de3fbb23d5a225e974d44829988b
Franz <fernandofranquiz@gmail.com>
2023-10-22 11:49:46 +0200

    Fix of theGameOfLife funtion

--- scripts/funtions.js
+++ scripts/funtions.js
@@ -1,16 +1,16 @@
-const arrayTable = [
-  [0, 0, 0, 1, 1, 0],
-  [0, 0, 1, 0, 1, 0],
-  [0, 0, 0, 1, 1, 0],
-  [0, 0, 0, 0, 0, 0],
-  [1, 1, 1, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
+const gameOfLifeTable = [
+  [1, 1, 1, 0, 1],
+  [0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 1],
+  [1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0],
 ];
-function celularCounter(xAxis, yAxis) {
+
+function celularCounter(xAxis, yAxis, arrayTable) {
   let counter1 = 0;
   for (let i = xAxis - 1; i <= xAxis + 1; i++) {
     for (let j = yAxis - 1; j <= yAxis + 1; j++) {
-      if (xAxis !== i || yAxis !== j) {
+      if (i !== xAxis || j !== yAxis) {
         if (arrayTable[i] && arrayTable[i][j] === 1) {
           counter1++;
         }
@@ -21,14 +21,30 @@ function celularCounter(xAxis, yAxis) {
   return counter1;
 }
 
-function theGameOfLife(arrayTable) {
-  const newArrayTable = strutureClone(arrayTable);
+/* Console.table(arrayTable);
+console.log(celularCounter(4, 1, arrayTable)); */
 
-  for (let i = 0; i < arrayTable.length; i++) {
-    for (let j =j < arrayTable.length; j++) {
-      ;
-      
+function theGameOfLife(arrayTable) {
+  const arrayTable2 = structuredClone(arrayTable);
+
+  const extent = arrayTable2.length;
+  let value;
+
+  for (let i = 0; i < extent; i++) {
+    for (let j = 0; j < extent; j++) {
+      value = celularCounter(i, j, arrayTable);
+      if (arrayTable[i][j] === 0) {
+        if (celularCounter(i, j, arrayTable) === 3) {
+          arrayTable2[i][j] = 1;
+        }
+      } else if (value < 2 || value > 3) {
+        arrayTable2[i][j] = 0;
+      }
     }
-    
   }
+
+  return arrayTable2;
 }
+
+console.table(gameOfLifeTable);
+console.table(theGameOfLife(gameOfLifeTable));
